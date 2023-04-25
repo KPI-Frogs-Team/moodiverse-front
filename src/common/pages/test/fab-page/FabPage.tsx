157f978ee25e7349b4ef956627433d42536b5fb9
Buiama <83916242+Buiama@users.noreply.github.com>
2023-04-25 16:33:56 +0200

    feature: added fab component (#29)

---
+++ src/common/pages/test/fab-page/FabPage.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Box } from "@mui/material";
+
+import Fab from "@/common/components/fab";
+
+import { wrapper } from "./FabPage.style";
+
+const FabPage: React.FC = () => (
+  <Box sx={wrapper}>
+    <Fab variant="add" color="primary" size="large" />
+    <Fab variant="edit" color="primary" size="large" />
+    <Fab variant="add" color="primary" size="medium" />
+    <Fab variant="edit" color="primary" size="medium" />
+    <Fab variant="add" color="primary" size="small" />
+    <Fab variant="edit" color="primary" size="small" />
+    <Fab variant="add" color="secondary" size="large" />
+    <Fab variant="edit" color="secondary" size="medium" />
+    <Fab variant="add" color="secondary" size="small" />
+  </Box>
+);
+
+export default FabPage;
