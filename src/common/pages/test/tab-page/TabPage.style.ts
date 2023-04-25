f5b6232f767bf6d5d5a84f3a46fcd885798363a1
HJK <89708817+HJyup@users.noreply.github.com>
2023-04-25 14:29:10 +0100

    feature/tab (#28)
    * feature: added tab-list
    
    * feature: added tab-panel
    
    * feature: added tab-page for testing
    
    * feature: added index.ts for importing tabs from one folder
    
    * fix: fixed colours and tag

---
+++ src/common/pages/test/tab-page/TabPage.style.ts
@@ -0,0 +1,6 @@
+import { SxProps, Theme } from "@mui/system";
+
+export const wrapper: SxProps<Theme> = {
+  width: "100vw",
+  minHeight: "100vh",
+};
---
+++ src/pages/test/tab.tsx
@@ -0,0 +1,5 @@
+import TabPage from "@/common/pages/test/tab-page";
+
+const Tab = () => <TabPage />;
+
+export default Tab;
